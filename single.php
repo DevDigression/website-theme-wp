c7c401e5a557eb6c96f6875ee8f0f46fef0ce622
Chris Rodgers <devdigression@gmail.com>
2018-10-17 15:13:10 -0400

    added post navigation links

--- single.php
+++ single.php
@@ -10,7 +10,12 @@
 	get_header();
 
 	while (have_posts()) {
-		the_post(); ?>
+		the_post(); 
+
+$next_post_url = get_permalink( get_adjacent_post(false,'',false)->ID );
+$previous_post_url = get_permalink( get_adjacent_post(false,'',true)->ID );
+
+?>
 
 <a class="blog-home-link" href="<?php echo site_url('/blog'); ?>">Blog Home</a>
 <?php if ( has_post_thumbnail() ) { ?>
@@ -27,6 +32,17 @@
   </div>
 
 <div class="post-content"><?php the_content(); ?></div>
+<div class="post-nav-links">
+
+<?php if (get_adjacent_post(false, '', true)): ?>
+	<a href="<?php echo get_permalink(get_adjacent_post(false,'',true)); ?>" class="post-nav-prev">PREVIOUS</a>
+<?php endif; ?>
+
+<?php if (get_adjacent_post(false, '', false)): ?>
+	<a href="<?php echo get_permalink(get_adjacent_post(false,'',false)); ?>" class="post-nav-next">NEXT</a>
+<?php endif; ?>
+
+</div>
 	<?php }
 
 	// get_footer();
